3ab50c984905e451acb69c162e5620e9214abdfc
mobilneWr26 <wiktor.rojek@uczen.mechaniktg.pl>
2026-04-16 22:11:03 +0200

    Rejestracja - Activity i Layout

--- app/src/main/java/com/example/projektaplikacjemobilne/RegisterActivity.java
+++ app/src/main/java/com/example/projektaplikacjemobilne/RegisterActivity.java
@@ -1,24 +1,51 @@
 package com.example.projektaplikacjemobilne;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText poleImie, poleLogin, poleHaslo;
+    Button przyciskZarejestruj;
+
+    BazaDanychZadania baza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        poleImie = findViewById(R.id.poleImie);
+        poleLogin = findViewById(R.id.poleLogin);
+        poleHaslo = findViewById(R.id.poleHaslo);
+        przyciskZarejestruj = findViewById(R.id.przyciskZarejestruj);
+
+        baza = new BazaDanychZadania(this);
+
+        przyciskZarejestruj.setOnClickListener(
+                new android.view.View.OnClickListener() {
+                    @Override
+                    public void onClick(android.view.View v) {
+                        dodajUzytkownika();
+                    }
+                });
+    }
+
+
+    private void dodajUzytkownika() {
+        String imie = poleImie.getText().toString();
+        String login = poleLogin.getText().toString();
+        String haslo = poleHaslo.getText().toString();
+
+        SQLiteDatabase db = baza.getWritableDatabase();
+        db.execSQL("INSERT INTO uzytkownicy(imie, login, haslo) VALUES(?,?,?)", new Object[]{imie, login, haslo});
+        Toast.makeText(this, "Zarejestrowano!", Toast.LENGTH_SHORT).show();
+
+        finish();
     }
 }
